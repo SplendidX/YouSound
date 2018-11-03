6948d0642780e8e0df81d79d6709ba5a3efab668
GaiusBolter <gaiusbolter@gmail.com>
2018-11-03 21:36:54 +0100

    Changed the code structure to utilise classes. Videos that do not fit the AUTHOR - TITLE template go to the directory/NONAME

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # YouSound
-Download and sort music from YouTube
+Download and sort music from YouTube based on the artist
+
+Supported template is AUTHOR - TITLE
+If the video doesn't fit into the tempalte it goes to the directory/NONAME
+You can change the directory YouSound downloads music to on Ln 38 "directory = ..."
 
 
 USE ONLY IF YOU ARE READY TO SACRIFICE YOUR MACHINE
+THIS SCRIPT USES SUBPROCESS COMMAND RM. IF EDITED WITHOUT CAUTION (or in case author messed something up) YOU MIGHT LOSE VALUABLE DATE / BREAK YOUR COMPUTER.
+
 
 
 In order for YouSound to work, you mus install pytube (pip install pytube) and ffmpeg (sudo apt-get install ffmpeg on debian, if you are using better distro than you will probably know what to do anyway).
--- YouSound.py
+++ YouSound.py
@@ -3,55 +3,88 @@ from pytube import YouTube
 import subprocess
 import re
 
-def getVideo():
-    try:
-        video = YouTube(input("Please insert video URL: "))
-        return video
-    except:
-        print("Invalid URL.")
-        getVideo()
 
-def getInfo():
-    author = video.title.split('-')[0]
-    author = author.strip(" ")
-    title = video.title.split('-')[1]
-    title = re.sub("[\(\[].*?[\)\]]", "", title)
-    title = title.strip(" ")
-    print("Downloading " + title + " by " + author)
-    return author, title
+class GetVideo:
+    def __init__(self):
+        self.video = input("Please insert video URL: ")
+        self.getobject()
+        self.getinfo()
+
+    def getobject(self):
+        try:
+            if self.video == "stop":
+                exit()
+            else:
+                self.video = YouTube(self.video)
+        except:
+            print("Invalid URL.")
+            self.getobject()
+
+    def getinfo(self):
+        if "-" in self.video.title:
+            self.author = self.video.title.split('-')[0]
+            self.author = self.author.strip(" ")
+            self.title = self.video.title.split('-')[1]
+            self.title = re.sub("[(\[].*?[)\]]", "", self.title)
+            self.title = self.title.strip(" ")
+        else:
+            self.title = self.video.title
+            self.title = re.sub("[(\[].*?[\)\]]", "", self.title)
+            self.title = self.title.strip(" ")
+
+
 
 def catalog():
     directory = os.getcwd()
 
-    if os.path.exists(str(os.path.join(directory,  author))):
-        saveDir = str(os.path.join(directory, author))
-    elif os.path.exists(str(os.path.join(directory, author.upper()))):
-        saveDir = str(os.path.join(directory, author.upper()))
-    elif os.path.exists(str(os.path.join(directory, author.lower()))):
-        saveDir = str(os.path.join(directory, author.lower()))
-    elif os.path.exists(str(os.path.join(directory,  author.capitalize()))):
-        saveDir = str(os.path.join(directory, author.capitalize()))
+    if "-" in ytobject.video.title:
+
+        if os.path.exists(str(os.path.join(directory,  ytobject.author))):
+            saveDir = str(os.path.join(directory, ytobject.author))
+
+        elif os.path.exists(str(os.path.join(directory, ytobject.author.upper()))):
+            saveDir = str(os.path.join(directory, ytobject.author.upper()))
+
+        elif os.path.exists(str(os.path.join(directory, ytobject.author.lower()))):
+            saveDir = str(os.path.join(directory, ytobject.author.lower()))
+
+        elif os.path.exists(str(os.path.join(directory,  ytobject.author.capitalize()))):
+            saveDir = str(os.path.join(directory, ytobject.author.capitalize()))
+
+        else:
+            os.makedirs(os.path.join(directory, ytobject.author))
+            saveDir = str(os.path.join(directory, ytobject.author))
     else:
-        os.makedirs(os.path.join(directory, author))
-        saveDir = str(os.path.join(directory, author))
+        if os.path.exists(str(os.path.join(directory,  "NONAME"))):
+            saveDir = str(os.path.join(directory, "NONAME"))
+        else:
+            os.makedirs(os.path.join(directory, "NONAME"))
+            saveDir = str(os.path.join(directory, "NONAME"))
 
     print(saveDir)
     return saveDir
 
+
+
+
 def download():
 
-    stream = video.streams.first()
+    stream = ytobject.video.streams.first()
     stream.download(saveDir)
 
 
+
 def convert():
-    commandconv = "ffmpeg -i \"" + os.path.join(saveDir, re.sub("\.", "" , video.title)) + "\".* -ab 160k -ac 2 -ar 44100 -vn \"" + os.path.join(saveDir, title) + "\".mp3"
-    commandrm = "rm \"" + os.path.join(saveDir, re.sub("\.", "" , video.title)) + "\".*"
+
+    commandconv = "ffmpeg -i \"" + os.path.join(saveDir, re.sub("\.", "" , ytobject.video.title)) + "\".mp4 -ab 160k -ac 2 -ar 44100 -vn \"" + os.path.join(saveDir, ytobject.title) + "\".mp3"
+    commandrm = "rm \"" + os.path.join(saveDir, re.sub("\.", "" , ytobject.video.title)) + "\".mp4"
     subprocess.call(commandconv, shell=True)
     subprocess.call(commandrm, shell=True)
 
-video = getVideo()
-author, title = getInfo()
-saveDir = catalog()
-download()
-convert()
+
+
+while True:
+    ytobject = GetVideo()
+    saveDir = catalog()
+    download()
+    convert()
