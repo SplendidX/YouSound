02fdfd54a36fc70070acbf6091692f4d9afba138
GaiusBolter <gaiusbolter@gmail.com>
2018-11-04 12:38:00 +0100

    Fixed a few PIP-8 errors

--- YouSound.py
+++ YouSound.py
@@ -3,14 +3,18 @@ from pytube import YouTube
 import subprocess
 import re
 
-#GetVideo class creates YouTube object from given URL, then performs simple actions to determine the author and title.
+# GetVideo class creates YouTube object from given URL, then performs simple actions to determine the author and title.
+
+
 class GetVideo:
     def __init__(self):
         self.video = input("Please insert video URL: ")
+        self.author = ""
+        self.title = ""
         self.getobject()
         self.getinfo()
 
-#If user types 'stop', the script quits. If the URL can't be used for creation of YouTube object, it prompts user to enter correct URL.
+# If the URL can't be used for creation of YouTube object, it prompts user to enter correct URL.
     def getobject(self):
         try:
             if self.video == "stop":
@@ -39,45 +43,49 @@ class GetVideo:
             self.title = self.title.strip(" ")
 
 
-#Checks if there already is a directory for specified artist, if not, creates it and makes it download directory for the song
-#If there is no author, check if there is NONAME directory, if not, crates it and makes it download directory for the song
+"""
+Checks if there already is a directory for specified artist, if not, 
+creates it and makes it download directory for the song
+If there is no author, check if there is NONAME directory, if not, 
+crates it and makes it download directory for the song
+"""
+
+
 def catalog():
     directory = os.getcwd()
 
     if "-" in ytobject.video.title:
 
         if os.path.exists(str(os.path.join(directory,  ytobject.author))):
-            saveDir = str(os.path.join(directory, ytobject.author))
+            save_dir = str(os.path.join(directory, ytobject.author))
 
         elif os.path.exists(str(os.path.join(directory, ytobject.author.upper()))):
-            saveDir = str(os.path.join(directory, ytobject.author.upper()))
+            save_dir = str(os.path.join(directory, ytobject.author.upper()))
 
         elif os.path.exists(str(os.path.join(directory, ytobject.author.lower()))):
-            saveDir = str(os.path.join(directory, ytobject.author.lower()))
+            save_dir = str(os.path.join(directory, ytobject.author.lower()))
 
         elif os.path.exists(str(os.path.join(directory,  ytobject.author.capitalize()))):
-            saveDir = str(os.path.join(directory, ytobject.author.capitalize()))
+            save_dir = str(os.path.join(directory, ytobject.author.capitalize()))
 
         else:
             os.makedirs(os.path.join(directory, ytobject.author))
-            saveDir = str(os.path.join(directory, ytobject.author))
+            save_dir = str(os.path.join(directory, ytobject.author))
     else:
         if os.path.exists(str(os.path.join(directory,  "NONAME"))):
-            saveDir = str(os.path.join(directory, "NONAME"))
+            save_dir = str(os.path.join(directory, "NONAME"))
         else:
             os.makedirs(os.path.join(directory, "NONAME"))
-            saveDir = str(os.path.join(directory, "NONAME"))
+            save_dir = str(os.path.join(directory, "NONAME"))
 
-    print(saveDir)
-    return saveDir
-
+    return save_dir
 
 
-#Takes the first stream possible and downloads it as a multimedia (video) file
+# Takes the first stream possible and downloads it as a multimedia (video) file
 def download():
 
     stream = ytobject.video.streams.first()
-    stream.download(saveDir)
+    stream.download(save_directory)
 
 
 """ Uses shell commands to extact the audio from multimedia file downloaded by function download() based on the
@@ -86,18 +94,19 @@ def download():
     
     Uses rm shell command to remove the clip. After extracting the audio it is no longer needed.
 """
+
+
 def convert():
 
-    commandconv = "ffmpeg -i \"" + os.path.join(saveDir, re.sub("\.", "" , ytobject.video.title)) + "\".mp4 -ab 160k -ac 2 -ar 44100 -vn \"" + os.path.join(saveDir, ytobject.title) + "\".mp3"
-    commandrm = "rm \"" + os.path.join(saveDir, re.sub("\.", "" , ytobject.video.title)) + "\".mp4"
+    commandconv = "ffmpeg -i \"" + os.path.join(save_directory, re.sub("\.", "" , ytobject.video.title)) + "\".mp4 -ab 160k -ac 2 -ar 44100 -vn \"" + os.path.join(save_directory, ytobject.title) + "\".mp3"
+    commandrm = "rm \"" + os.path.join(save_directory, re.sub("\.", "" , ytobject.video.title)) + "\".mp4"
     subprocess.call(commandconv, shell=True)
     subprocess.call(commandrm, shell=True)
 
 
-
 while True:
     ytobject = GetVideo()
-    saveDir = catalog()
+    save_directory = catalog()
     download()
     convert()
 
